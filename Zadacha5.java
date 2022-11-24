85f5a2983fd759609a79b34418be6edd0d9c6b4a
Haru <harumolodec@mail.ru>
2022-11-24 12:21:54 +0700

    Zadacha5 END

---
+++ Zadacha5.java
@@ -0,0 +1,27 @@
+package DZ1;
+
+public class Zadacha5 {
+    public boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (!Character.isLetterOrDigit(s.charAt(l))) {
+                l++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(s.charAt(r))) {
+                r--;
+                continue;
+            }
+            if (Character.toLowerCase(s.charAt(l)) == Character.toLowerCase(s.charAt(r))) {
+                l++;
+                r--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
